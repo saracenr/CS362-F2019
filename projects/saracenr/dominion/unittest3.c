bd26be765f436e854b87795df7ff20334af851ab
saracenr <saracenr@oregonstate.edu>
2019-11-10 15:46:45 -0800

    Update unittest3.c

--- projects/saracenr/dominion/unittest3.c
+++ projects/saracenr/dominion/unittest3.c
@@ -40,68 +40,70 @@ int main() {
             {
             for (int choice = -1; choice <= 3; choice++)
             	{
-#if (NOISY_TEST == 1)
-    printf("Test player %d with %d card(s). Ambassador at pos %d.\n", p, handCount, ambassadorLocation);
-#endif
-	                memset(&G, 23, sizeof(struct gameState));   // clear the game state
-	                r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
-	                for (int eachPlayer = 0; eachPlayer < 4; eachPlayer++) {  // Sets number of cards for each player
-	                	G.handCount[eachPlayer] = handCount;
-	                }
-	                G.hand[p][ambassadorLocation] = ambassador;
-	                for (int otherCards = 0; otherCards <= handCount; otherCards++) {
-	                	if (otherCards != ambassadorLocation) {
-	                		G.hand[p][otherCards] = k[otherCards];
-	                	}
-	                }
-
-	                for (int eachPlayer = 0; eachPlayer < 4; eachPlayer++) {  // Sets number of cards for each player
-	                	if (p != eachPlayer) {
-	                		for (int otherCards = 0; otherCards <= handCount; otherCards++) {
-	                		G.hand[eachPlayer][otherCards] = k[otherCards];
-	                		}
-	                	}
-	                }
-
-	                int successful = ambassadorCard(handCount,choice, p, &G, ambassadorLocation);
+            		for (int revealedCard = 1; revealedCard <= handCount) {
 	#if (NOISY_TEST == 1)
-	                printf("Handsize is %d, Ambassador is at pos %d. They chose to reveal %d card copies.\n", handCount, ambassadorLocation, choice);
+	    printf("Test player %d with %d card(s). Ambassador at pos %d.\n", p, handCount, ambassadorLocation);
+	#endif
+		                memset(&G, 23, sizeof(struct gameState));   // clear the game state
+		                r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
+		                for (int eachPlayer = 0; eachPlayer < 4; eachPlayer++) {  // Sets number of cards for each player
+		                	G.handCount[eachPlayer] = handCount;
+		                }
+		                G.hand[p][ambassadorLocation] = ambassador;
+		                for (int otherCards = 0; otherCards <= handCount; otherCards++) {
+		                	if (otherCards != ambassadorLocation) {
+		                		G.hand[p][otherCards] = k[otherCards];
+		                	}
+		                }
+
+		                for (int eachPlayer = 0; eachPlayer < 4; eachPlayer++) {  // Sets number of cards for each player
+		                	if (p != eachPlayer) {
+		                		for (int otherCards = 0; otherCards <= handCount; otherCards++) {
+		                		G.hand[eachPlayer][otherCards] = k[otherCards];
+		                		}
+		                	}
+		                }
+
+		                int successful = ambassadorCard(revealedCard,choice, p, &G, ambassadorLocation);
+	#if (NOISY_TEST == 1)
+		printf("Handsize is %d, Ambassador is at pos %d. They chose to reveal %d card copies.\n", handCount, ambassadorLocation, choice);
 	#endif
 
-	                //  Assertions
-	                if (choice > 2 || choice < 0  && successful != -1) {  //  Assert the function returns -1 if the choice is invalid
-	                	printf("Choice was invalid but the card function was successful!\n");
-	                }
-	                
-	                if (choice == 0 && G.handCount[p] != handCount-1 && successful != -1) {  // Assert that the ambassador card was discarded and no cards gained
-	                	printf("Hand for current player is not correct for successful returning 0 cards!\n");
-	                }
-	                else if (choice == 1 && G.handCount[p] != handCount-2 && successful != -1) {   // Assert that the ambassador card was discarded and no cards gained
-	                	printf("Hand for current player is not correct for successful returning 1 cards!\n");
-	                }
-	                else if (choice == 2 && G.handCount[p] != handCount-3 && successful != -1) {   // Assert that the ambassador card was discarded and no cards gained
-	                	printf("Hand for current player is not correct for successful returning 2 cards!\n");
-	                }
+		                //  Assertions
+		                if (choice > 2 || choice < 0  && successful != -1) {  //  Assert the function returns -1 if the choice is invalid
+		                	printf("Choice was invalid but the card function was successful!\n");
+		                }
 
-	                // Assert that cannot reveal ambassador card played to its own effect
-	                if (handCount == ambassadorLocation && successful != -1) { 
-	                	printf("Player revealed the played ambassador card for its effect, this is incorrect!\n");
-	                }
+		                if (choice == 0 && G.handCount[p] != handCount-1 && successful != -1) {  // Assert that the ambassador card was discarded and no cards gained
+		                	printf("Hand for current player is not correct for successful returning 0 cards!\n");
+		                }
+		                else if (choice == 1 && G.handCount[p] != handCount-2 && successful != -1) {   // Assert that the ambassador card was discarded and no cards gained
+		                	printf("Hand for current player is not correct for successful returning 1 cards!\n");
+		                }
+		                else if (choice == 2 && G.handCount[p] != handCount-3 && successful != -1) {   // Assert that the ambassador card was discarded and no cards gained
+		                	printf("Hand for current player is not correct for successful returning 2 cards!\n");
+		                }
 
-	                //  Asserts each opponent gained the correct amount of cards.
-	                for (int eachPlayer = 0; eachPlayer < 4; eachPlayer++) {  // Checks each players hand
-	                	if (p != eachPlayer) {
-			                if (choice == 0 && G.handCount[eachPlayer] != handCount && successful != -1) {  //  Assert that the number of cards gained for each player is correct
-			                	printf("Hand for opponent player is not correct for successful returning 0 cards!\n");
-			                }
-			                else if (choice == 1 && G.handCount[p] != handCount+1 && successful != -1) {   //  Assert that the number of cards gained for each player is correct
-			                	printf("Hand for opponent player is not correct for successful returning 1 cards!\n");
-			                }
-			                else if (choice == 2 && G.handCount[p] != handCount+2 && successful != -1) {   //  Assert that the number of cards gained for each player is correct
-			                	printf("Hand for opponent player is not correct for successful returning 2 cards!\n");
-			                }
+		                // Assert that cannot reveal ambassador card played to its own effect
+		                if (handCount == ambassadorLocation && successful != -1) { 
+		                	printf("Player revealed the played ambassador card for its effect, this is incorrect!\n");
+		                }
+
+		                //  Asserts each opponent gained the correct amount of cards.
+		                for (int eachPlayer = 0; eachPlayer < 4; eachPlayer++) {  // Checks each players hand
+		                	if (p != eachPlayer) {
+				                if (choice == 0 && G.handCount[eachPlayer] != handCount && successful != -1) {  //  Assert that the number of cards gained for each player is correct
+				                	printf("Hand for opponent player is not correct for successful returning 0 cards!\n");
+				                }
+				                else if (choice == 1 && G.handCount[p] != handCount+1 && successful != -1) {   //  Assert that the number of cards gained for each player is correct
+				                	printf("Hand for opponent player is not correct for successful returning 1 cards!\n");
+				                }
+				                else if (choice == 2 && G.handCount[p] != handCount+2 && successful != -1) {   //  Assert that the number of cards gained for each player is correct
+				                	printf("Hand for opponent player is not correct for successful returning 2 cards!\n");
+				                }
+				            }
 			            }
-		            }
+			        }
 	            }
 	        }
 	    }
