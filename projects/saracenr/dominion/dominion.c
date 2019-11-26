259f0ede84f6dac3fb0604829104fd6ddeca06ae
saracenr <saracenr@oregonstate.edu>
2019-11-25 17:32:53 -0800

    Update dominion.c

--- projects/saracenr/dominion/dominion.c
+++ projects/saracenr/dominion/dominion.c
@@ -807,7 +807,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
             }
         }
-
+        printf("I made it past the loop!\n");
         //Reset Hand
         for (i = 0; i <= state->handCount[currentPlayer]; i++) {
             state->hand[currentPlayer][i] = temphand[i];
