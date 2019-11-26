e5e4f865bbe5cfef785241d68d23e1619af38ddc
saracenr <saracenr@oregonstate.edu>
2019-11-25 18:30:09 -0800

    Update unittestBug6.c

--- projects/saracenr/dominion/unittestBug6.c
+++ projects/saracenr/dominion/unittestBug6.c
@@ -43,19 +43,17 @@ int main() {
         int currentCoins = G.coins;
         G.hand[p][0] = feast;
 
-        printf("The last card in the discard is: %d\n", G.discard[p][10]);
         cardEffect(feast, testCards[eachCard], 0, 0, &G, 0, &maxBonus);
-        printf("The last card in the discard is: %d\n", G.discard[p][10]);
 
         if (currentCoins != G.coins) {
             printf("The number of coins for the player is incorrect!\n");
         }
 
-        if (G.discard[p][G.discardCount[p]] != testCards[eachCard] && testCards[eachCard] != gold) {
+        if (G.discard[p][10] != testCards[eachCard] && testCards[eachCard] != gold) {
             printf("The player did not gain the card and it was a valid card choice.\n");
         }
 
-        if (G.discard[p][G.discardCount[p]] != testCards[eachCard] && testCards[eachCard] == gold) {
+        if (G.discard[p][10] != testCards[eachCard] && testCards[eachCard] == gold) {
             printf("The player gained a card when it was an invalid choice of card.\n");
         }
     }
