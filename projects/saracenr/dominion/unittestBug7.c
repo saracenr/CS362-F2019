f256895ad33a552fca8f68f68fcec398bc74475f
saracenr <saracenr@oregonstate.edu>
2019-12-02 14:14:37 -0800

    Update unittestBug7.c

--- projects/saracenr/dominion/unittestBug7.c
+++ projects/saracenr/dominion/unittestBug7.c
@@ -95,7 +95,8 @@ int main() {
 
                 printf("I made it to calling the function!");
                 cardEffect(tribute, 0, 0, 0, &G, 0, &maxBonus);
-
+                printf("deckSize: %d, discardSize: %d, dups: %d.\n", deckSize, discardSize, duplicateReveal);
+                
                 if (noCardsRevealed == 1) {
                     printf("Player had no deck or discard to reveal!\n");
                     if (currentCoins != G.coins) {
