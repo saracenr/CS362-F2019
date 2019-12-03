efe9c34856ae04d2e9c48c160c3d6e600fd97f2f
saracenr <saracenr@oregonstate.edu>
2019-12-02 17:01:32 -0800

    Update

--- projects/saracenr/dominion/unittestBug8.c
+++ projects/saracenr/dominion/unittestBug8.c
@@ -17,24 +17,22 @@ int main() {
     struct gameState G;
 
 
-    for (int eachCard = 0; eachCard < 3; eachCard++) {
-        memset(&G, 23, sizeof(struct gameState));   // clear the game state
-        r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
-        G.whoseTurn = p;
-        G.handCount[p] = 5; // set the number of cards on hand
+    memset(&G, 23, sizeof(struct gameState));   // clear the game state
+    r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
+    G.whoseTurn = p;
+    G.handCount[p] = 5; // set the number of cards on hand
 
-        G.coins = 0;
-        int currentCoins = G.coins;
-        G.hand[p][0] = minion;
+    G.coins = 0;
+    int currentCoins = G.coins;
+    G.hand[p][0] = minion;
 
-        cardEffect(minion, 1, 0, 0, &G, 0, &maxBonus);
+    cardEffect(minion, 1, 0, 0, &G, 0, &maxBonus);
 
-        if (currentCoins + 2  != G.coins) {
-            printf("The number of coins for the player is incorrect!\n");
-        }
+    if (currentCoins + 2  != G.coins) {
+        printf("The number of coins for the player is incorrect!\n");
+    }
 
-        if (maxBonus != 2) {
-            printf("Bonus was not correctly updated!.\n");
-        }
+    if (maxBonus != 2) {
+        printf("Bonus was not correctly updated!.\n");
     }
 }
